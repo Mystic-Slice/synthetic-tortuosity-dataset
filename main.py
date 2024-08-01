da3bcd39eea02c4b8ea49304fde0ff78c648d99e
Mystic-Slice <vaashwath@gmail.com>
2024-08-01 10:47:43 +0530

    final configuration

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import shutil
 import math 
 from Walker import Walker
 from util import bound, generate_centered_point, rotate_vector
-from config import GRID_SIZE, NUM_WALKERS, WALKER_INITIAL_REPRODUCTION_PROBABILITY
+from config import GRID_SIZE, NUM_TORTUOUS_WALKERS, NUM_WALKERS, WALKER_INITIAL_REPRODUCTION_PROBABILITY
 
 def generate_image(tortuous_image):
     img = [[[0, 0, 0] for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
@@ -18,6 +18,11 @@ def generate_image(tortuous_image):
     
     # Initialize walkers
     walkers = []
+    if tortuous_image:
+        walker_is_tortuous = [False] * (NUM_WALKERS - NUM_TORTUOUS_WALKERS) + [True] * NUM_TORTUOUS_WALKERS
+        np.random.shuffle(walker_is_tortuous)
+    else:
+        walker_is_tortuous = [False] * NUM_WALKERS
     for i in range(NUM_WALKERS):
         # Slightly move the walker away from the initial point in all directions uniformly
         # This is because, the vector field force direction is undefined at the source
@@ -36,7 +41,7 @@ def generate_image(tortuous_image):
         walker = Walker(
                 grid = img, 
                 grid_size = GRID_SIZE,
-                tortuous = tortuous_image,
+                tortuous = walker_is_tortuous[i],
                 reproduction_probability = WALKER_INITIAL_REPRODUCTION_PROBABILITY,
                 initial_point = start_point,
                 source_point = SOURCE_POINT,
@@ -59,28 +64,31 @@ def generate_image(tortuous_image):
         if w.tortuous:
             tortuous_points += w.get_tortuous_points()
 
-    # Calculate the bounding box for the tortuous regions
-    bounding_box_coords = []
-    for points_set in tortuous_points:
-        max_x = max([p[0] for p in points_set])
-        max_x = bound(GRID_SIZE, math.floor(max_x + GRID_SIZE * 0.01))
-        min_x = min([p[0] for p in points_set])
-        min_x = bound(GRID_SIZE, math.floor(min_x - GRID_SIZE * 0.01))
+    tortuous_points = sum([list(point_set) for point_set in tortuous_points], [])
+    tortuous_points = list(set(tortuous_points))
 
-        max_y = max([p[1] for p in points_set])
-        max_y = bound(GRID_SIZE, math.floor(max_y + GRID_SIZE * 0.01))
-        min_y = min([p[1] for p in points_set])
-        min_y = bound(GRID_SIZE, math.floor(min_y - GRID_SIZE * 0.01))
+    # # Calculate the bounding box for the tortuous regions
+    # bounding_box_coords = []
+    # for points_set in tortuous_points:
+    #     max_x = max([p[0] for p in points_set])
+    #     max_x = bound(GRID_SIZE, math.floor(max_x + GRID_SIZE * 0.01))
+    #     min_x = min([p[0] for p in points_set])
+    #     min_x = bound(GRID_SIZE, math.floor(min_x - GRID_SIZE * 0.01))
 
-        # Uncomment to draw the bounding box
-        # for x in range(min_x, max_x + 1):
-        #     img[x][min_y] = [255, 0, 0]
-        #     img[x][max_y] = [255, 0, 0]
+    #     max_y = max([p[1] for p in points_set])
+    #     max_y = bound(GRID_SIZE, math.floor(max_y + GRID_SIZE * 0.01))
+    #     min_y = min([p[1] for p in points_set])
+    #     min_y = bound(GRID_SIZE, math.floor(min_y - GRID_SIZE * 0.01))
+
+    #     # Uncomment to draw the bounding box
+    #     for x in range(min_x, max_x + 1):
+    #         img[x][min_y] = [255, 0, 0]
+    #         img[x][max_y] = [255, 0, 0]
         
-        # for y in range(min_y, max_y + 1):
-        #     img[min_x][y] = [255, 0, 0]
-        #     img[max_x][y] = [255, 0, 0]
-        bounding_box_coords.append([(min_x, min_y), (max_x, max_y)])
+    #     for y in range(min_y, max_y + 1):
+    #         img[min_x][y] = [255, 0, 0]
+    #         img[max_x][y] = [255, 0, 0]
+    #     bounding_box_coords.append([(min_x, min_y), (max_x, max_y)])
 
     # Write the image to a file
     img = np.array(img, dtype=np.uint8)
@@ -90,9 +98,16 @@ def generate_image(tortuous_image):
     end_index = int(GRID_SIZE * 0.8)
     img = img[start_index:end_index, start_index:end_index]
 
+    # Remove the corresponding tortuous points
+    tortuous_points = [
+        (x-start_index, y-start_index) for x, y in tortuous_points 
+        if start_index <= x < end_index and start_index <= y < end_index
+    ]
+
+
     img = Image.fromarray(img)
 
-    return img, bounding_box_coords
+    return img, tortuous_points
 
 DEBUG = 0
 if DEBUG:
@@ -113,24 +128,24 @@ else:
 
     print("Generating Tortuous Images")
     for i in tqdm.tqdm(range(NUM_IMAGES_PER_CLASS)):
-        img, bounding_box_coords = generate_image(True)
+        img, tortuous_points = generate_image(True)
         filename = f"images/tortuous/{i}.png"
         files[num_images] = {
             "filename": filename, 
             "tortuous": 1,
-            "bounding_box_coords": bounding_box_coords
+            "tortuous_points": tortuous_points
         }
         img.save(filename)
         num_images += 1
 
     print("Generating Non-Tortuous Images")
     for i in tqdm.tqdm(range(NUM_IMAGES_PER_CLASS)):
-        img, bounding_box_coords = generate_image(False)
+        img, tortuous_points = generate_image(False)
         filename = f"images/non_tortuous/{i}.png"
         files[num_images] = {
             "filename": filename,
             "tortuous": 0,
-            "bounding_box_coords": bounding_box_coords
+            "tortuous_points": tortuous_points
         }
         img.save(filename)
         num_images += 1
